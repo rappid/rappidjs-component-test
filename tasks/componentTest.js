13e914f2434cca32deadee3b748768a8b8f1299f
Tony Findeisen <github@it-ony.de>
2014-07-10 22:50:22 +0200

    added phantomjs support

--- tasks/componentTest.js
+++ tasks/componentTest.js
@@ -6,11 +6,90 @@ module.exports = function (grunt) {
         'Starts a web server for running component tests again', function () {
 
             var options = this.options(this.data);
-            var done = this.async();
+            var done = this.async(),
+                page;
 
-            require('..')(options, function() {
-                done();
-            });
+            var server = require('..')(options, grunt.log.debug);
+            var url = "http://localhost:" + options.port;
+
+            if (options.runOnPhantom === true) {
+                var phantom = require('phantom');
+
+                phantom.create(function (ph) {
+                    ph.createPage(function (p) {
+                        page = p;
+                        page.open(url, function (status) {
+
+                            if (status === "success") {
+                                getTestResult(function(testResult) {
+                                    ph.exit();
+
+                                    if (testResult.failedTests.length === 0 &&
+                                        testResult.errorTests.length === 0) {
+                                        grunt.log.ok(testResult.passedTests.length + " tests passed.");
+
+                                        server.close(function() {
+                                            done();
+                                        }) ;
+
+                                    } else {
+
+                                        [
+                                            {
+                                                message: "failed",
+                                                tests: testResult.failedTests
+                                            },
+                                            {
+                                                message: "errored",
+                                                tests: testResult.errorTests
+                                            }
+                                        ]
+                                        .forEach(function (obj) {
+                                                if (obj.tests.length) {
+                                                    grunt.log.error(obj.tests.length + " tests " + obj.message + ":");
+
+                                                    obj.tests.forEach(function(test) {
+                                                        grunt.log.error("\t" + test.title +
+                                                            (test.err ? ": " + test.err.message || test.err : ""))
+                                                    });
+                                                }
+                                        });
+
+                                        server.close(function () {
+                                            done(false);
+                                        });
+
+                                    }
+                                });
+
+                            } else {
+                                server.close(function () {
+                                    done(false);
+                                });
+                            }
+                        });
+                    });
+                });
+            } else {
+                grunt.log.ok("Open " + url + " to run tests and see the result");
+            }
+
+
+            function getTestResult(callback) {
+                checkStatus();
+
+                function checkStatus() {
+                    page.evaluate(function () {
+                        return window.testsCompleted && window.testResults;
+                    }, function(result) {
+                        if (result) {
+                            callback && callback(result);
+                        } else {
+                            setTimeout(checkStatus, 1000);
+                        }
+                    })
+                }
+            }
 
         });
 
